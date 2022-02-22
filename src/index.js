385eadabaa6ad6c9c88395576a8effe24fb44ef8
Eugen Kovalchuk <kovalchuk.eak@gmail.com>
2022-02-22 17:24:37 +0200

    Without errors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./app/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import AuthProvider from './contexts/auth/Provider';
+import 'modern-normalize/modern-normalize.css';
+import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
